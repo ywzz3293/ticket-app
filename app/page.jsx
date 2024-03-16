a53bf8ff62feb291ba1464b0acf99ccc6f866e4b
ywzz3293 <50302188+ywzz3293@users.noreply.github.com>
2024-03-16 14:40:33 -0700

    fix minor issue of display

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
 import TicketCard from "./(components)/TicketCard";
-
 const Dashboard = () => {
   return (
-    <div className="p-5 ">
-      <div className="">
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TicketCard />
         <TicketCard />
         <TicketCard />
         <TicketCard />
